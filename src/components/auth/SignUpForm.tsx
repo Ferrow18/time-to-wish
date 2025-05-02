256021f37e035adec09e692c41ae576e4b672ef5
Ferrow <gerhat.fr@gmail.com>
2025-05-02 23:17:14 +0200

    add isSubmitSuccessful check and text changes to SignUpForm

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -11,7 +11,7 @@ export const SignUpForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful },
   } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
@@ -22,36 +22,50 @@ export const SignUpForm = () => {
     console.log(user);
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-      <input
-        type="text"
-        placeholder="Name"
-        className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
-        {...register("name", { required: true })}
-      />
-      {errors.name && <span className="text-red-500">Name is required</span>}
-      <input
-        type="email"
-        placeholder="Email"
-        className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
-        {...register("email", { required: true })}
-      />
-      {errors.email && <span className="text-red-500">Email is required</span>}
-      <input
-        type="password"
-        placeholder="Password"
-        className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
-        {...register("password", { required: true })}
-      />
-      {errors.password && (
-        <span className="text-red-500">Password is required</span>
+    <>
+      {isSubmitSuccessful ? (
+        <p className="text-center">
+          Thank you for signing up! Verify your email to get started.
+        </p>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Name"
+            className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+            {...register("name", { required: true })}
+          />
+          {errors.name && (
+            <span className="text-red-500">Name is required</span>
+          )}
+          <input
+            type="email"
+            placeholder="Email"
+            className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <span className="text-red-500">Email is required</span>
+          )}
+          <input
+            type="password"
+            placeholder="Password"
+            className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+            {...register("password", { required: true, minLength: 6 })}
+          />
+          {errors.password && (
+            <span className="text-red-500">
+              Password is required and must be at least 6 characters long
+            </span>
+          )}
+          <button
+            type="submit"
+            className="cursor-pointer rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+          >
+            Sign Up
+          </button>
+        </form>
       )}
-      <button
-        type="submit"
-        className="cursor-pointer rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
-      >
-        Sign Up
-      </button>
-    </form>
+    </>
   );
 };
