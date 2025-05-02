48ac08e766a7671316160ddc427e8b4eef49ce56
Ferrow <gerhat.fr@gmail.com>
2025-05-02 20:19:31 +0200

    add basic sign up form

---
+++ src/components/auth/SignUpForm.tsx
@@ -0,0 +1,50 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export const SignUpForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="Name"
+        className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+        {...register("name", { required: true })}
+      />
+      {errors.name && <span className="text-red-500">Name is required</span>}
+      <input
+        type="email"
+        placeholder="Email"
+        className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+        {...register("email", { required: true })}
+      />
+      {errors.email && <span className="text-red-500">Email is required</span>}
+      <input
+        type="password"
+        placeholder="Password"
+        className="rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+        {...register("password", { required: true })}
+      />
+      {errors.password && (
+        <span className="text-red-500">Password is required</span>
+      )}
+      <button
+        type="submit"
+        className="cursor-pointer rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200"
+      >
+        Sign Up
+      </button>
+    </form>
+  );
+};
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,10 @@
+import { SignUpForm } from "../components/auth/SignUpForm";
+
 export const SignUp = () => {
   return (
     <div className="container mx-auto flex min-h-[calc(100vh-112px)] flex-col items-center justify-center gap-8">
       <h1 className="text-4xl">Sign Up page</h1>
+      <SignUpForm />
     </div>
   );
 };
