203de65ac9d74eb69aedea703e4c9d58f4eb484f
Ferrow <gerhat.fr@gmail.com>
2025-05-02 19:36:11 +0200

    add text and buttons to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,21 @@
 export const Hero = () => {
   return (
-    <section className="container mx-auto">
-      <h1 className="text-4xl">Hero</h1>
+    <section className="container mx-auto flex min-h-[calc(100vh-112px)] flex-col items-center justify-center gap-8">
+      <div className="space-y-4 text-center">
+        <h1 className="text-4xl">Welcome to the Time To Wish</h1>
+        <p className="text-xl">
+          Time To Wish is a place where you can store all your birthdays and
+          wish for them.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <button className="cursor-pointer rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200">
+          Sign In
+        </button>
+        <button className="cursor-pointer rounded-full bg-white px-4 py-2 text-black transition-colors hover:bg-amber-200">
+          Sign Up
+        </button>
+      </div>
     </section>
   );
 };
