b093f349d66dabf579c60e0d080f4ef294b9e9eb
Ferrow <gerhat.fr@gmail.com>
2025-05-03 19:09:36 +0200

    add calendar to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,50 @@
-import { useContext } from "react";
-import { UserContext } from "../context/contexts";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+
+const birthdays = [
+  {
+    name: "John Doe",
+    date: new Date("2025-05-01"),
+  },
+  {
+    name: "Jane Doe",
+    date: new Date("2025-05-02"),
+  },
+  {
+    name: "Ferrowec",
+    date: new Date("2025-05-05"),
+  },
+];
 
 export const Dashboard = () => {
-  const { user } = useContext(UserContext);
-  console.log("user from dashboard: ", user);
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
   return (
     <div className="container mx-auto flex min-h-[calc(100vh-120px)] flex-col items-center justify-center gap-8">
       <h1>Dashboard</h1>
-      <p>Current user: {user?.email}</p>
+      <div className="flex gap-20">
+        <div className="h-[330px]">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-md border shadow"
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h2>Upcoming Birthdays</h2>
+          <ul>
+            {birthdays.map((birthday, index) => (
+              <li key={index} className="flex items-center gap-2">
+                <span className="text-sm text-muted-foreground">
+                  {birthday.date.toLocaleDateString()}
+                </span>
+                <span className="font-medium">{birthday.name}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
